5f5519a81fea9e3bbfc4d28053d0fa64c6fdd504
SanthoshPalanivel <33660552+SanthoshPalanivel@users.noreply.github.com>
2017-11-14 23:20:00 +0530

    Add files via upload

---
+++ DemoSample1.java
@@ -0,0 +1,10 @@
+
+public class DemoSample1 {
+public static void main(String[] args) {
+	System.out.println("First Test case");
+	System.out.println("First Test case2");
+	System.out.println("First Test case3");
+	System.out.println("First Test case4");
+	System.out.println("First Test case5");
+}
+}
